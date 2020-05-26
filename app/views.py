4c08ecf8b0666d86a0a25b8982bb8ec19ad3f518
marcos-bah <marcos.barbosa@unifei.edu.br>
2020-05-25 22:50:03 -0300

    add conexao com banco de dados mysql

--- app/views.py
+++ app/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TarefaForm
+from .entidades.tarefa import Tarefa
+from .services import tarefa_services
 
 # Create your views here.
 
 def listar_tarefas(req):
     nome_tarefa = "Assitir a semana Python e Django da TreinaWeb"
     return render(req, 'tarefas/listar_tarefas.html', {"nome_tarefa": nome_tarefa})
+
+def cadastrar_tarefa(req):
+    if req.method == "POST":
+        form_tarefa = TarefaForm(req.POST)
+        if form_tarefa.is_valid():
+            titulo = form_tarefa.cleaned_data["titulo"]
+            descricao = form_tarefa.cleaned_data["descricao"]
+            data_expiracao = form_tarefa.cleaned_data["data_expiracao"]
+            prioridade = form_tarefa.cleaned_data["prioridade"]
+
+            tarefa_nova = Tarefa(titulo=titulo, descricao=descricao, data_expiracao=data_expiracao, prioridade=prioridade)
+
+            tarefa_services.cadastrar_tarefa(tarefa_nova)
+
+            return redirect('listar_tarefas')
+    else:
+        form_tarefa = TarefaForm()
+    return render(req, 'tarefas/form_tarefa.html', {"form_tarefa": form_tarefa})
